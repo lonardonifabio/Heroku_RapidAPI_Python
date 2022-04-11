4c7c800a58d9d068ad157032497e005e39ab89d4
Fabio <lonardoni.fabio@gmail.com>
2022-04-11 22:40:43 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,51 +1,36 @@
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+import os
+import requests
 from forms import UrlSearchForm
-from werkzeug.datastructures import Authorization
-from wtforms import Form, StringField
+from flask_sqlalchemy import SQLAlchemy
+
 from newspaper import Article
 from wordcloud import WordCloud
 from textblob import TextBlob
+
 import base64
 import io
 import datetime
-import os
-import requests
 
-app = Flask(__name__) 
-
-ENV = 'dev'
-if ENV == 'dev':
-    app.debug = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pdvopmbizwkwfn:8d748dcfd72c2963abea2783e02ce10d8a4481d7682f5137942ed3356315ec5d@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/dbb1sldknq385f'
-else:
-    app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pdvopmbizwkwfn:8d748dcfd72c2963abea2783e02ce10d8a4481d7682f5137942ed3356315ec5d@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/dbb1sldknq385f'
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pdvopmbizwkwfn:8d748dcfd72c2963abea2783e02ce10d8a4481d7682f5137942ed3356315ec5d@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/dbb1sldknq385f'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
-class UrlSearchForm(Form):
-    search = StringField('')
-
 class News(db.Model):
-    __tablename__ = 'news'
+    __tablename__ = 'News'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200))
     author = db.Column(db.String(200))
     link = db.Column(db.String(1000), unique=True)
     summary = db.Column(db.String(2000))
     tag = db.Column(db.String(200))
-    sentiment = db.Column(db.Float)
+    sentiment = db.Column(db.String(1000))
     imgLnk = db.Column(db.String(1000))
-    rank = db.Column(db.Integer)
-    fake = db.Column(db.String(1))
-    customer = db.Column(db.String(200), unique=True)
-    email = db.Column(db.String(200))
     date = db.Column(db.String(200))
+
     
-    def __init__(self, title, author, link, summary, tag, sentiment, imglink, rank, fake, customer, email, date):
+    def __init__(self, title, author, link, summary, tag, sentiment, imglink, date):
         self.title = title
         self.author = author 
         self.link = link
@@ -53,41 +38,7 @@ class News(db.Model):
         self.tag = tag
         self.sentiment = sentiment
         self.imgLnk = imglink
-        self.rank = rank
-        self.fake = fake
-        self.customer = customer
-        self.email = email
         self.date = date
-
-@app.route('/')
-def index():
-    return render_template('index.html', form = urlsearch, errors = errors)
-
-@app.route('/submitTo', methods=['POST'])
-def submitTo():
-    if request.method == 'POST':
-        customer = request.form['customer']
-        fake = request.form['fake']
-        email = request.form['email']
-        rank = request.form['rank']
-        if db.session.query(news).filter(news.customer == customer).count() == 0:
-            data = news(title, author, link, summary, tag, sentiment, imglink, rank, fake, customer, email, date)
-            db.session.add(data)
-            db.session.commit()
-        return render_template('success.html')
-
-@app.route('/submit', methods=['POST'])
-def submit():
-    errors = []
-    urlsearch = UrlSearchForm(request.form)
-    if request.method == "POST":
-        try:
-            return search_results(urlsearch)
-        except:
-            errors.append(
-                "Please enter the URL of your news article."
-            )       
-    return render_template("index.html", form = urlsearch, errors = errors)
 
 def get_wordcloud(text):
     pil_img = WordCloud(max_font_size=50, max_words=100, background_color="white").generate(text=text).to_image()
@@ -97,9 +48,31 @@ def get_wordcloud(text):
     img_b64 = base64.b64encode(img.getvalue()).decode()
     return img_b64
 
+
+
+#from app import db
+#db.create_all()
+#db.session.commit()
+
+@app.route('/', methods = ["GET", "POST"])
+def index():
+    errors = []
+    urlsearch = UrlSearchForm(request.form)
+    #return search_results(urlsearch)
+    if request.method == "POST":
+        try:
+            return search_results(urlsearch)
+        except:
+            errors.append(
+                "Please enter the URL of your news article."
+            )       
+    return render_template("index.html", form = urlsearch, errors = errors)
+
+
 def search_results(urlsearch):
     urlsearch = UrlSearchForm(request.form)
     search_string = urlsearch.data['search']
+    link = search_string
     article = Article(search_string)
     article.download()
     article.parse()
@@ -110,16 +83,36 @@ def search_results(urlsearch):
     published_date = date.strftime("%d %B %Y")
     author = article.authors[0]
     image = article.top_image
+    imglink = image
     cloud = get_wordcloud(data)
     keyword = article.keywords
+    tag = keyword
     summary = article.summary
     blob = TextBlob(data)
     sentiment = blob.sentiment
+    #print ("Fields")
+    #print (title)
+    #print (author)
+    #print (link)
+    #print (summary)
+    #print (tag)
+    #print (sentiment)
+    #print (imglink)
+    #print (date)
+    if link == '':
+        #print("first")
+        return render_template("index.html", form = urlsearch, errors = errors)
+    #if db.session.query(News).filter(News.link == search_string).count() == 0:
+    dataexport = News(title, author, link, summary, tag, sentiment, imglink, date)
+    #print("second")
+    #print (dataexport)
+    db.session.add(dataexport)
+    #print ("DB-before commit")
+    db.session.commit()
+    #print ("commit")
     return render_template("results.html", search_string = search_string, title = title, summary = summary, image = image, published_date=published_date, author = author, keyword = keyword, cloud = cloud, sentiment = sentiment)
-
-@app.route('/TryAgain', methods=['POST'])
-def TryAgain():
-    return render_template("index.html")
+    #return render_template("index.html", form = urlsearch, errors = errors)
 
 if __name__ == '__main__':
-    app.run()
+      app.run()
+
